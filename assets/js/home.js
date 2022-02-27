fe651a5d7581ad5ceac736281ddc30e2fd99c3be
toiwilou <toiwilouhassane@gmail.com>
2022-02-28 00:17:40 +0100

    deploy

---
+++ assets/js/home.js
@@ -0,0 +1,13 @@
+import '../bootstrap';
+import '../styles/home.css';
+
+const homeHeaderContent = document.querySelector('#home-header .content');
+const textAbout = document.querySelector('#home-header .item .text-about');
+const textContent = document.querySelector('#home-header .item .text-about .content');
+
+//const hAbout
+
+const windowWidth = window.innerWidth;
+//homeHeaderContent.style.width = windowWidth * 0.7 + 'px';
+
+//window.onresize = fun;
